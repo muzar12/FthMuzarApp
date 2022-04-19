ab27a7f50b40ba41317e1ad3d80093b77864ae61
David Muzar <david.rokomet@gmail.com>
2022-04-19 22:47:38 +0200

    upload photo working

--- App.js
+++ App.js
@@ -2,6 +2,18 @@ import React from 'react';
 
 import Routes from './navigation/index';
 
+import { LogBox } from 'react-native';
+import _ from 'lodash';
+
+/*LogBox.ignoreLogs(['Warning:...']); // ignore specific logs
+LogBox.ignoreAllLogs(); // ignore all logs
+const _console = _.clone(console);
+console.warn = message => {
+if (message.indexOf('Setting a timer') <= -1) {
+   _console.warn(message);
+   }
+};*/
+
 export default function App() {
   return <Routes />;
 }
--- screens/UploadScreen.js
+++ screens/UploadScreen.js
@@ -1,205 +1,138 @@
-import * as ImagePicker from "expo-image-picker";
-import Firebase from '../config/firebase';
-import React from "react";
+import * as React from "react";
 import {
-  ActivityIndicator,
-  Button,
-  Image,
-  Share,
-  StatusBar,
-  StyleSheet,
   Text,
   View,
-  LogBox,
+  StyleSheet,
+  Image,
+  Platform,
+  useWindowDimensions,
 } from "react-native";
-import * as Clipboard from "expo-clipboard";
-import uuid from "uuid";
+import Constants from "expo-constants";
+import firebase from "../config/firebase";
+import { AntDesign, Feather } from "@expo/vector-icons";
+import uplodImageFromDevice from "../photoUpload/uploadImageFromDevice";
+import getBlobFromUri from "../photoUpload/getBlobFromUri";
+import manageFileUpload from "../photoUpload/manageFileUpload";
 
-const up = Firebase.storage()
+export default function App() {
+  const [imgURI, setImageURI] = React.useState(null);
 
-// Firebase sets some timeers for a long period, which will trigger some warnings. Let's turn that off for this example
-LogBox.ignoreLogs([`Setting a timer for a long period`]);
+  const [isUploading, setIsUploading] = React.useState(false);
+  const [progress, setProgress] = React.useState(0);
+  const [remoteURL, setRemoteURL] = React.useState("");
 
-export default class App extends React.Component {
-  state = {
-    image: null,
-    uploading: false,
+  const [error, setError] = React.useState(null);
+  const { width } = useWindowDimensions();
+
+  const handleLocalImageUpload = async () => {
+    const fileURI = await uplodImageFromDevice();
+
+    if (fileURI) {
+      setImageURI(fileURI);
+    }
   };
 
-  async componentDidMount() {
-    if (Platform.OS !== "web") {
-      const {
-        status,
-      } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-      if (status !== "granted") {
-        alert("Sorry, we need camera roll permissions to make this work!");
-      }
-    }
-  }
+  const onStart = () => {
+    setIsUploading(true);
+  };
 
-  render() {
-    let { image } = this.state;
+  const onProgress = (progress) => {
+    setProgress(progress);
+  };
+  const onComplete = (fileUrl) => {
+    setRemoteURL(fileUrl);
+    setIsUploading(false);
+    setImageURI(null);
+  };
 
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        {!!image && (
-          <Text
-            style={{
-              fontSize: 20,
-              marginBottom: 20,
-              textAlign: "center",
-              marginHorizontal: 15,
-            }}
-          >
-            Example: Upload ImagePicker result
+  const onFail = (error) => {
+    setError(error);
+    setIsUploading(false);
+  };
+  const handleCloudImageUpload = async () => {
+    if (!imgURI) return;
+
+    let fileToUpload = null;
+
+    const blob = await getBlobFromUri(imgURI);
+
+    await manageFileUpload(blob, { onStart, onProgress, onComplete, onFail });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Attach and upload image</Text>
+      {Boolean(imgURI) && (
+        <View>
+          <Image
+            source={{ uri: imgURI }}
+            resizeMode="contain"
+            style={{ width, height: width }}
+          />
+        </View>
+      )}
+
+      {!isUploading && (
+        <View style={styles.row}>
+          <AntDesign
+            name="addfile"
+            size={36}
+            color={imgURI ? "green" : "black"}
+            onPress={handleLocalImageUpload}
+          />
+
+          {Boolean(imgURI) && (
+            <Feather
+              name="upload-cloud"
+              size={36}
+              color="black"
+              onPress={handleCloudImageUpload}
+            />
+          )}
+        </View>
+      )}
+
+      {isUploading && (
+        <View style={styles.uploadProgressContainer}>
+          <Text> Upload {progress} of 100% </Text>
+        </View>
+      )}
+
+      {Boolean(remoteURL) && (
+        <View style={{ paddingVertical: 20 }}>
+          <Text>
+            Image has been uploaded at
+            <Text style={{ color: "blue" }}> {remoteURL} </Text>
           </Text>
-        )}
-
-        <Button
-          onPress={this._pickImage}
-          title="Izberi sliko iz galerije"
-        />
-
-        <Button onPress={this._takePhoto} title="Ustvari sliko" />
-        
-        {this._maybeRenderImage()}
-        {this._maybeRenderUploadingOverlay()}
-
-        <StatusBar barStyle="default" />
-      </View>
-    );
-  }
-
-  _maybeRenderUploadingOverlay = () => {
-    if (this.state.uploading) {
-      return (
-        <View
-          style={[
-            StyleSheet.absoluteFill,
-            {
-              backgroundColor: "rgba(0,0,0,0.4)",
-              alignItems: "center",
-              justifyContent: "center",
-            },
-          ]}
-        >
-          <ActivityIndicator color="#fff" animating size="large" />
         </View>
-      );
-    }
-  };
-
-  _maybeRenderImage = () => {
-    let { image } = this.state;
-    if (!image) {
-      return;
-    }
-
-    return (
-      <View
-        style={{
-          marginTop: 30,
-          width: 250,
-          borderRadius: 3,
-          elevation: 2,
-        }}
-      >
-        <View
-          style={{
-            borderTopRightRadius: 3,
-            borderTopLeftRadius: 3,
-            shadowColor: "rgba(0,0,0,1)",
-            shadowOpacity: 0.2,
-            shadowOffset: { width: 4, height: 4 },
-            shadowRadius: 5,
-            overflow: "hidden",
-          }}
-        >
-          <Image source={{ uri: image }} style={{ width: 250, height: 250 }} />
-        </View>
-        <Text
-          onPress={this._copyToClipboard}
-          onLongPress={this._share}
-          style={{ paddingVertical: 10, paddingHorizontal: 10 }}
-        >
-          {image}
-        </Text>
-      </View>
-    );
-  };
-
-  _share = () => {
-    Share.share({
-      message: this.state.image,
-      title: "Check out this photo",
-      url: this.state.image,
-    });
-  };
-
-  _copyToClipboard = () => {
-    Clipboard.setString(this.state.image);
-    alert("Copied image URL to clipboard");
-  };
-
-  _takePhoto = async () => {
-    let pickerResult = await ImagePicker.launchCameraAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-    });
-
-    this._handleImagePicked(pickerResult);
-  };
-
-  _pickImage = async () => {
-    let pickerResult = await ImagePicker.launchImageLibraryAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
-    });
-
-    console.log({ pickerResult });
-
-    this._handleImagePicked(pickerResult);
-  };
-
-  _handleImagePicked = async (pickerResult) => {
-    try {
-      this.setState({ uploading: true });
-
-      if (!pickerResult.cancelled) {
-        const uploadUrl = await uploadImageAsync(pickerResult.uri);
-        this.setState({ image: uploadUrl });
-      }
-    } catch (e) {
-      console.log(e);
-      alert("Upload failed, sorry :(");
-    } finally {
-      this.setState({ uploading: false });
-    }
-  };
+      )}
+    </View>
+  );
 }
 
-async function uploadImageAsync(uri) {
-  // Why are we using XMLHttpRequest? See:
-  // https://github.com/expo/expo/issues/2402#issuecomment-443726662
-  const blob = await new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-      resolve(xhr.response);
-    };
-    xhr.onerror = function (e) {
-      console.log(e);
-      reject(new TypeError("Network request failed"));
-    };
-    xhr.responseType = "blob";
-    xhr.open("GET", uri, true);
-    xhr.send(null);
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
 
-  const fileRef = up.ref(up.getStorage(), uuid.v4());
-  const result = await up.uploadBytes(fileRef, blob);
-
-  // We're done with the blob, close and release it
-  blob.close();
-
-  return await up.getDownloadURL(fileRef);
-}
+  row: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    flexDirection: "row",
+  },
+  uploadProgressContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  heading: {
+    margin: 20,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
